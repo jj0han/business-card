37db17a1900dd39a8b58db23babd2031bfea1283
jj0han <joaovieirabomfim@gmail.com>
2022-07-20 22:45:26 -0400

    first commit

---
+++ src/components/Info.js
@@ -0,0 +1,20 @@
+import React from "react"
+import '../App.css'
+
+export default function Info() {
+    return(
+        <div className="Info__container">
+            <img className="Info__img"/>
+            <h1 className="Info__title">João Bomfim</h1>
+            <h3 className="Info__sub-title">Front-End Developer</h3>
+            <a className="Info__site" href="https://jj0han.github.io/portfolio/">jj0han.github.io/portfolio</a>
+            <nav className="Info__nav">
+                <ul className="Info__list">
+                    <li className="Info__item">
+                    <a className="Info__link" href="#"><img className="Info__logo" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAcVJREFUOE9jZKAQMFKonwFuwGKfvCIuNqYDwesmnMNn6IqZmkYM/387RGTc6QOpAxuw1CXptsLDYypfOYQYRKRE6gwtN7UyNjD8QzfoRJJRgXzY1X4Jpp8M297I3PaOeqIGMcA+9ofCs9PsIPZfZV0Gjb8Pjv39w5Amuf/0VZDYB28bwV8/f06/pf8t3NodLMSw54voT9eg1xxgA5IKj10MOFSnJ/zpCcMfZT0GzT/3Gf7/Z/jFwMDQwfj//4n/zAyzGP8zytw0/MZg43qV4c4/Loa4T/qXjocd1wcb4Nj2ZT/b7x8OrmenMJj9uvZH4899FmzhADLgvsN7hqwfWgxf/jMeYAjZ7Qg3gPE/gwNIk+mL/fHF1xsTGBgYHVEM+cfwutOca0qvMUcjRPwfdgOY/v133F3Le/CNs0nqPwaGbkZGRj6G/ww7GZn+JImmiasxMP7fT9CAPbW8B0CK3jhZSv9n+msvsufUckYGhv8Mqz0cSDIAIxxoZoBT85eVDEwMYSAbQWEA8wJ+FzCsZAjZGQGOBYeGVzzMrJymDP8YmX+y/Dl7pFLgPdbkvNRbkIHjjzEovTH8/XmaIezAF+plJnJzJQDsqtkRA1+BsAAAAABJRU5ErkJggg==" alt="Gmail"/> Gmail</a></li>
+                    <li className="Info__item"><a className="Info__link" href="https://www.linkedin.com/in/jo%C3%A3o-bomfim-957a04237"><img className="Info__logo" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/PjxzdmcgZGF0YS1uYW1lPSJMYXllciAxIiBpZD0iTGF5ZXJfMSIgdmlld0JveD0iMCAwIDUxMiA1MTIiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHRpdGxlLz48cmVjdCBmaWxsPSIjMmE2NmJjIiBoZWlnaHQ9IjQxMi4yMiIgcng9IjU1LjQzIiB3aWR0aD0iNDEyLjIyIiB4PSI0OS44OSIgeT0iNDkuODkiLz48cmVjdCBmaWxsPSIjZmVmZWZlIiBoZWlnaHQ9IjE1OC4wOCIgd2lkdGg9IjQ4LjkiIHg9IjEzOC41NiIgeT0iMjE2Ii8+PGNpcmNsZSBjeD0iMTYzLjAxIiBjeT0iMTYyLjU4IiBmaWxsPSIjZmVmZWZlIiByPSIyNi40NiIvPjxwYXRoIGQ9Ik0zMzcuNiwyMTcuMThzLTEzLjYtNC40MS0zNC43OC0xLjQxYTQ1LjYzLDQ1LjYzLDAsMCwwLTIxLjIzLDguNDksMzUuNDcsMzUuNDcsMCwwLDAtMTIuMjUsMTYuMTlWMjE2aC00OC45VjM3NC4wOGg0OC45VjI4OGMwLTEzLjA1LDctMjUuNTksMTguODMtMzFhMzEuNzIsMzEuNzIsMCwwLDEsMTAuMTQtMi43NnMxMS4xNC0yLjIzLDIwLjksOC43NmM1LjI5LDYsNy43MywxMy45NCw3LjczLDIxLjkxdjg5LjIzaDQ4LjlWMjY0LjlDMzc4LDIyNSwzMzcuNiwyMTcuMTgsMzM3LjYsMjE3LjE4WiIgZmlsbD0iI2ZlZmVmZSIvPjwvc3ZnPg==" alt="LinkedIn"/> LinkedIn</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
---
+++ src/components/Interests.js
@@ -0,0 +1,11 @@
+import React from "react"
+import '../App.css';
+
+export default function Interests() {
+    return (
+        <div>
+            <h2 className="sub-title">Interests</h2>
+            <p className="simple-txt">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+        </div>
+    )
+}
